52a2e00da0e69add6884c2c6cba48510a020e003
plattine <loghindan@gmail.com>
2026-02-11 15:21:39 +0100

    commit....

--- src/OwnExercises/SheepCounterTest.java
+++
@@ -1,8 +0,0 @@
-package OwnExercises;
-
-public class SheepCounterTest {
-    public static void main(String[] args) {
-        SheepCounter counter = new SheepCounter(49);
-        counter.countSheep();
-    }
-}
